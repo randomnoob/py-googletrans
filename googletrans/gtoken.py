65224701cd962a05117ea0ece92ab90a2f969d5b
randomnoob <tuantt13@wru.vn>
2018-09-21 14:23:33 +0700

    Update gtoken.py

--- googletrans/gtoken.py
+++ googletrans/gtoken.py
@@ -56,13 +56,12 @@ class TokenAcquirer(object):
             return
 
         r = self.session.get(self.host)
-        
+
         raw_tkk = self.RE_RAWTKK.search(r.text)
         if raw_tkk:
             self.tkk = raw_tkk.group(1)
             return
-        
-        
+
         # this will be the same as python code after stripping out a reserved word 'var'
         code = unicode(self.RE_TKK.search(r.text).group(1)).replace('var ', '')
         # unescape special ascii characters such like a \x3d(=)
